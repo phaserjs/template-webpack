4fd882c02a16ec256c7a63e03c5cc44c46472c1f
photonstorm <rich@photonstorm.com>
2017-09-25 15:28:18 +0100

    Updated for Beta 4 + added renderer flags.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
     },
 
     plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
     ]
 
 };
