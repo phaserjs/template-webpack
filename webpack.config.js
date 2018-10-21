2f23407ac2b8a668197390c485ebfbc2e3ecc33e
Rory O'Connell <19547+RoryO@users.noreply.github.com>
2018-10-21 16:16:01 -0700

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,52 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-
-    entry: './src/index.js',
-
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
+    entry: {
+        index: './src/index.js'
     },
 
     module: {
         rules: [
           {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
+              test: [ /\.vert$/, /\.frag$/ ],
+              use: 'raw-loader'
+          },
+
+          {
+              test: /\.js$/,
+              include: path.resolve(__dirname, 'src/'),
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['env']
+                  }
+              }
           }
         ]
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all',
+                    priority: 1
+                }
+            }
+        }
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: '[name].js'
+    },
+
+    performance: {
+        hints: false
     },
 
     plugins: [
@@ -27,6 +57,5 @@ module.exports = {
             'CANVAS_RENDERER': JSON.stringify(true),
             'WEBGL_RENDERER': JSON.stringify(true)
         })
-    ]
-
+    ],
 };
