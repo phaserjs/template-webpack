de3f661d0e5c9c4829fbf9347af6e320bb909805
Caleb Taylor <caleb.taylor@nike.com>
2019-01-07 17:11:12 -0800

    Upgrade to Webpack 4 (including webpack-dev-server). Add ES6 support via Babel 7.

---
+++ webpack/prod.js
@@ -0,0 +1,23 @@
+const merge = require("webpack-merge");
+const path = require("path");
+const base = require("./base");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(base, {
+  mode: "production",
+  output: {
+    filename: "bundle.min.js"
+  },
+  devtool: false,
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  }
+});
