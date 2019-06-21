7864fc3e8980855d7b0fa432b96c98e595d3ca74
RELEPHANT\d.traka <d.traka@emakina.nl>
2019-06-21 13:04:18 +0300

    first push and phaser update

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 
 const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+    this.load.image("logo", logoImg);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+    const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+    this.tweens.add({
+        targets: logo,
+        y: 450,
+        duration: 2000,
+        ease: "Power2",
+        yoyo: true,
+        loop: -1
+    });
 }
