7c360cb23d84f92656669ab7c463037774e2614d
William Colby <wbcolby@gmail.com>
2019-07-08 22:33:27 -0400

    7 - Keyboard Controls
    Up through step 7 of  Phaser 3 tutorial.
    Continue: https://phaser.io/tutorials/making-your-first-phaser-3-game/part8

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,111 @@
 import "phaser";
 // @ts-ignore // this works via webpack file-loader
-import logoImg from "./assets/logo.png";
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
-  backgroundColor: '#efefef',
+  physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+  },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
+var platforms;
 
-// const imageKey = "logo";
-const cartmanSvgKey = "cartman";
+var player;
+
+var cursors;
 
 function preload() {
-  // this.load.image(imageKey, logoImg);
-  this.load.svg(cartmanSvgKey, 'src/assets/cartman.svg', {
-    width: 416,
-    height: 388,
-  });
+
+  this.load.image('sky', 'src/assets/sky.png');
+  this.load.image('ground', 'src/assets/platform.png');
+  this.load.image('star', 'src/assets/star.png');
+  this.load.image('bomb', 'src/assets/bomb.png');
+
+  this.load.spritesheet('dude',
+      'src/assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+  );
+
 }
 
 function create() {
-  // const logo = this.add.image(400, 150, imageKey);
-  const cartman = this.add.image(400, 150, cartmanSvgKey);
 
-  this.tweens.add({
-    // targets: logo,
-    targets: cartman,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  this.add.image(400, 300, 'sky');
+
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+
+  this.physics.add.collider(player, platforms);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
   });
+
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+  });
+
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+}
+
+function update(){
+  if (cursors.left.isDown)
+  {
+      player.setVelocityX(-160);
+
+      player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown)
+  {
+      player.setVelocityX(160);
+
+      player.anims.play('right', true);
+  }
+  else
+  {
+      player.setVelocityX(0);
+
+      player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down)
+  {
+      player.setVelocityY(-330);
+  }
 }
