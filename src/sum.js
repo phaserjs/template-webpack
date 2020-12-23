dddca5d221deadd973ab80322d5262e1af944223
Bee Godwin <bee.godwin@bbc.co.uk>
2020-12-23 19:37:07 +0000

    es6 syntax for jest

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,3 @@
-function sum (a, b) {return a + b};
+const sum = (a, b) => a + b;
 
-module.exports = sum;
+export default sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require("../src/sum");
+import sum from "../src/sum";
 
 test("test", () => {
     expect(sum(1, 2)).toBe(3);
