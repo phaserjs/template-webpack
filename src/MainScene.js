cd4d3f2f0aaea98dd9f5dd612e575b40bbfa7e4a
Guy <guymoustapha@gmail.com>
2021-01-20 16:20:45 +0200

    add assets necessary

--- src/MainScene.js
+++ src/MainScene.js
@@ -9,5 +9,31 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     console.log('create')
+    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world);
+    this.inputKeys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    })
+  }
+
+  update() {
+    const speed= 2.5;
+    let playerVelocity = new Phaser.Math.Vector2();
+    if(this.inputKeys.left.isDown) {
+      playerVelocity.x = -1;
+    }
+    else if(this.inputKeys.right.isDown) {
+      playerVelocity.x = 1
+    }
+    else if(this.inputKeys.up.isDown) {
+      playerVelocity.y = -1
+    }
+    else if(this.inputKeys.down.isDown) {
+      playerVelocity.y = 1
+    }
+    playerVelocity.scale(speed);
+    this.player.setVelocity(playerVelocity.x, playerVelocity.y)
   }
 }
