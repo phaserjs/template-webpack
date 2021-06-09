565a2bb0bd73209b0cc7ebf462c9df85f77c1621
R Springer <rspringer@rocmondriaan.nl>
2021-06-09 09:54:51 +0200

    Updated the condingstyle using the basic prettier settings

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+class MyGame extends Phaser.Scene {
+  constructor() {
+    super();
+  }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyGame,
 };
 
 const game = new Phaser.Game(config);
