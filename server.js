ac0b5081f23f88960588fbe162ce333e86b3f6bf
Deniz Traka <deniz.traka@deptagency.com>
2021-08-15 18:35:58 +0200

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
