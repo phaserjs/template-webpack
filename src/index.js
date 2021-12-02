61f6ea7650ee7632dfe1d617ab494302d8e530d3
sean <seansiddells@gmail.com>
2021-12-03 11:22:19 +1300

    reorganise assets folder, updated export and json

--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
-import Phaser from 'phaser';
-import water from './assets/country-level/WaterTextures.png'
-import map from './assets/country-level/test2.json'
+// import Phaser from 'phaser';
+// import water from './assets/country-level/WaterTextures.png'
+// import map from './assets/country-level/test2.json'
 
 
-class MyGame extends Phaser.Scene
-{
-    constructor (key)
-    {
-        super(key);
-    }
+// class MyGame extends Phaser.Scene
+// {
+//     constructor (key)
+//     {
+//         super(key);
+//     }
 
-    preload ()
-    {
-      this.load.image("water", water, {
-        frameWidth: 16,
-        frameHeight: 16
-      });
-      this.load.tilemapTiledJSON("map", map, {
-        frameWidth: 16,
-        frameHeight: 16
-      });
-    }
+//     preload ()
+//     {
+//       this.load.image("water", water, {
+//         frameWidth: 16,
+//         frameHeight: 16
+//       });
+//       this.load.tilemapTiledJSON("map", map, {
+//         frameWidth: 16,
+//         frameHeight: 16
+//       });
+//     }
 
-    create ()
-    {
-      // this.add.image(0,0, 'grass')
-      const map = this.make.tilemap({key:'map'});
-      const tileset = map.addTilesetImage("tiles", "water", 16, 16);
-      console.log(tileset);
-      map.createLayer( "water", tileset, 400, 400);
-      console.log(map.layers);
-      this.scene.start('game')
-    }
-}
+//     create ()
+//     {
+//       // this.add.image(0,0, 'grass')
+//       const map = this.make.tilemap({key:'map'});
+//       const tileset = map.addTilesetImage("tiles", "water", 16, 16);
+//       console.log(tileset);
+//       map.createLayer( "water", tileset);
+//       console.log(map.layers);
+//     }
+// }
 
-const config = {
-    type: Phaser.WEBGL,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
-};
+// const config = {
+//     type: Phaser.WEBGL,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     scene: MyGame
+// };
 
-const game = new Phaser.Game(config);
+// const game = new Phaser.Game(config);
