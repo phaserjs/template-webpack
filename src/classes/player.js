56c9934b39ea18d80eb42e8308a88067fc9787b8
joshuawc <joshkosh99@gmail.com>
2021-12-03 19:31:02 +1300

    Add deceleration logic

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,4 +1,5 @@
 import { Actor } from "./actor";
+import { Math } from "phaser";
 
 export class Player extends Actor {
     constructor(scene, x, y) {
@@ -9,14 +10,23 @@ export class Player extends Actor {
         this.keyS = this.scene.input.keyboard.addKey('S')
         this.keyD = this.scene.input.keyboard.addKey('D')
 
-         this.setScale(0.5)
+        this.setScale(0.5)
 
         this.body.setSize(85, 100)
         this.body.setOffset(73, 45)
-        
+
+
+
 
         this.initAnimations()
     }
+
+    fire() {
+        console.log('Yah yeert');
+        console.log(this.body.touching.down);
+    }
+
+
 
     initAnimations() {
         this.scene.anims.create({
@@ -40,29 +50,41 @@ export class Player extends Actor {
 
     update() {
 
+        //   console.log(Math.Angle.Between(this.body.x, this.body.y, this.scene.input.mousePointer.x, this.scene.input.mousePointer.y));
+        // this.rotation = Math.Angle.Between(this.body.x, this.body.y, this.scene.input.mousePointer.x, this.scene.input.mousePointer.y)
+        // this.body.setVelocityX(0)
 
-        this.body.setVelocityX(0)
+        if (this.body.velocity.x > 0) {
+
+            this.body.velocity.x -= 10
+        } else if (this.body.velocity.x < 0) {
+            this.body.velocity.x += 10
+        }
+
+        if (this.scene.input.mousePointer.isDown) {
+            this.fire()
+        }
 
         if (this.keyW.isDown) {
-            this.body.velocity.y = -100;
+            this.body.velocity.y = -300;
         }
 
         if (this.keyA.isDown) {
             this.anims.play('run', true)
-            this.body.velocity.x = -110;
+            this.body.velocity.x = -330;
             this.checkFlip();
-            
+
         }
         else if (this.keyD.isDown) {
             this.anims.play('run', true)
-            this.body.velocity.x = 110
+            this.body.velocity.x = 330
             this.checkFlip();
-            
+
 
         }
         else {
             this.anims.play('idle', true)
-            
+
         }
     }
 }
