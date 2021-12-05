9bdb0f105ef2f36b4a225eefbd16527cba0d8143
bz888 <ziyu.ben158@gmail.com>
2021-12-05 20:54:39 +1300

    add atk unfucked

--- src/scenes/loading.js
+++ src/scenes/loading.js
@@ -51,7 +51,7 @@ export class LoadingScene extends Scene {
     this.load.atlas('enemy', 'sprites/anims/testBoss.png', 'sprites/atlas/enemy-1-atlas.json')
     this.load.atlas('player', 'sprites/anims/wizard-sheet.png', 'sprites/atlas/wizard.json')
     this.load.atlas('viking', 'sprites/anims/viking2.png', 'sprites/atlas/viking-atlas.json')
-    this.load.atlas('sushi-hands', 'sprites/anims/hand-sushi-boss.png', 'sprites/atlas/hand-sushi-boss-atlas.json')
+    this.load.atlas('sushi-hands', 'sprites/anims/sushi-hands.png', 'sprites/atlas/hand-sushi-boss-atlas.json')
   }
 
   create () {
