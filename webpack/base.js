19a6fa2b260ff01c7cf11853347847a73be45a7c
joshuawc <joshkosh99@gmail.com>
2021-12-06 15:22:57 +1300

    Actually read things;

--- webpack/base.js
+++ webpack/base.js
@@ -48,7 +48,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: './dist/index.html'
+      template: './index.html'
     })
   ]
 }
