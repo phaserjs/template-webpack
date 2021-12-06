6deb386678d6d227953604b04ea52f188d11d0ec
joshuawc <joshkosh99@gmail.com>
2021-12-06 15:01:14 +1300

    Hopefully fix it

---
+++ webpack/config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: '../src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../', 'dist')
+  },
+  module: {
+    rules: [
+
+    ]
+  }
+}
