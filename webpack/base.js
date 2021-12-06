476348783a2e537c4d3767c957fcf8bcbe231815
joshuawc <joshkosh99@gmail.com>
2021-12-06 17:48:18 +1300

    The docs will save me

--- webpack/base.js
+++ webpack/base.js
@@ -43,8 +43,8 @@ module.exports = {
     new CopyWebpackPlugin(
       [
         {
-          from: path.resolve(__dirname, '**', '*', 'assets'),
-          to: path.resolve(__dirname, 'dist')
+          from: 'assets',
+          to: 'dist'
         }
       ]
     ),
