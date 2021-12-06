62ef064baa8dad500683014d8816f8dc267cb162
joshuawc <joshkosh99@gmail.com>
2021-12-06 15:11:24 +1300

    Revert

--- webpack/base.js
+++ webpack/base.js
@@ -48,7 +48,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: './src/dist/index.html'
+      template: './src/index.html'
     })
   ]
 }
