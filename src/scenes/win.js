a1a1b50a57bdc88ac72d5e201b53c1064c3e41c0
sean <seansiddells@gmail.com>
2021-12-08 18:10:51 +1300

    remove credits button from win scene, redirects to credits

--- src/scenes/win.js
+++ src/scenes/win.js
@@ -18,33 +18,15 @@ export class Win extends Scene {
     this.add.image(480, 250, 'win-text').setScale(0.8)
     const banana = this.add.sprite(100, 330, 'banana').setScale(0.3).setInteractive()
     const arrow = this.add.sprite(900, 480, 'arrow').setScale(0.1).setInteractive()
-    const credit = this.add.sprite(875, 30, 'credits-button').setScale(0.15).setInteractive()
 
     // Detect when pointer is hovering over button, and change scene on click
     banana.on('pointerdown', () => {
       banana.destroy()
       this.add.text(100, 450, 'You disrespectful fool! You murdered our banana!', { fontFamily: 'Press Start 2P', fontsize: '40px' })
     })
-    credit.on('pointerdown', () =>
-      this.scene.start('credits-scene'), console.log('loading credits'))
-
-    credit.on('pointerover', () =>
-      credit.setTint(0xff0000a160))
-
-    fore.on('pointerover', () =>
-      credit.clearTint())
-
-    tree.on('pointerover', () =>
-      credit.clearTint())
-
-    mount.on('pointerover', () =>
-      credit.clearTint())
-
-    farMount.on('pointerover', () =>
-      credit.clearTint())
 
     arrow.on('pointerdown', () =>
-      this.scene.start('title-scene'), console.log('title'))
+      this.scene.start('credits-scene'), console.log('title'))
 
     arrow.on('pointerover', () =>
       arrow.setTint(0xff0000a160))
