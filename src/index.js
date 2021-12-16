3e171c56f2aaaf00c1c00f57eb55700b44e08fd0
Cinderrr <86509478+Cinderrr@users.noreply.github.com>
2021-12-16 20:13:38 +0100

    Moving keystrokes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import noteSheet from './components/noteSheet.json';
 
 const config = {
     type: Phaser.AUTO,
@@ -15,75 +15,244 @@ const config = {
 };
 
 var keys;
-
-var r1;
-var r2;
-var r3;
-var r4;
-var r5;
-var r6;
-var r7;
-
+var keyPositions = [];
+var keyStrokes = []; // 2d array note contains [object, timetable]
+var timeline;
 
 const game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('arrow', 'assets/sprites/arrow.png');
+    noteSheet.aNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(60, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0xFF0000);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    noteSheet.wNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(170, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0xFFA500);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    noteSheet.dNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(280, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0xFFFF00);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    noteSheet.spaceNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(500, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0x00FF00);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    noteSheet.jNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(720, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0x0000FF);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    noteSheet.iNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(830, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0x4B0082);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+    
+    noteSheet.lNotes.forEach((note) => {
+        var currentTimeline;
+        var currentPosition;
+
+
+        currentPosition = this.add.rectangle(940, 1000, 100, 100, 0x6666ff);
+        currentPosition.setStrokeStyle(3, 0x8F00FF);
+        currentTimeline = this.tweens.createTimeline({  })
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 100,
+            duration: 500,
+            ease: 'Cubic.easeOut',
+            delay: (note.time * 60)
+        });
+
+        currentTimeline.add({
+            targets: currentPosition,
+            y: 1000,
+            duration: 500,
+            ease: 'Quint.easeIn',
+            delay: 0
+        });
+        keyStrokes.push({currentPosition, currentTimeline, "time": note.time })
+    });
+
+    keyStrokes.sort(function (a, b) {
+        return a.time - b.time;
+      });
 }
 
 function create ()
 {
-
+    /*********************************KEY SETUP*********************************/
     keys = this.input.keyboard.addKeys('A, W, D, SPACE, J, I, L');
 
-    r1 = this.add.rectangle(60, 600, 100, 100, 0x6666ff);
-    r2 = this.add.rectangle(170, 600, 100, 100, 0x6666ff);
-    r3 = this.add.rectangle(280, 600, 100, 100, 0x6666ff);
+    keyPositions[0] = this.add.rectangle(60, 600, 100, 100, 0x6666ff);
+    keyPositions[1] = this.add.rectangle(170, 600, 100, 100, 0x6666ff);
+    keyPositions[2] = this.add.rectangle(280, 600, 100, 100, 0x6666ff);
 
-    r4 = this.add.rectangle(500, 600, 300, 100, 0x6666ff);
+    keyPositions[3] = this.add.rectangle(500, 600, 300, 100, 0x6666ff);
 
-    r5 = this.add.rectangle(720, 600, 100, 100, 0x6666ff);
-    r6 = this.add.rectangle(830, 600, 100, 100, 0x6666ff);
-    r7 = this.add.rectangle(940, 600, 100, 100, 0x6666ff);
+    keyPositions[4] = this.add.rectangle(720, 600, 100, 100, 0x6666ff);
+    keyPositions[5] = this.add.rectangle(830, 600, 100, 100, 0x6666ff);
+    keyPositions[6] = this.add.rectangle(940, 600, 100, 100, 0x6666ff);
+    /*********************************KEY SETUP*********************************/
 
+//    dPosition = this.add.rectangle(600, 200, 148, 148, 0x1a65ac);
+//    dPosition.setStrokeStyle(30, 0xfa65ac);
 
-
-
-
-
-
-
-
-
-
-//    r3 = this.add.rectangle(600, 200, 148, 148, 0x1a65ac);
-//    r3.setStrokeStyle(30, 0xfa65ac);
-
-
-
-    /*this.tweens.add({
-        targets: r1,
-        y: 100,
-        duration: 3000,
-        ease: 'Power4',
-        yoyo: true,
-        repeat: true,
-        delay: 1000
-    });*/
+    keyStrokes.forEach((note) => {
+        note.currentTimeline.play();
+    });
+    //timeline.play()
 }
 
 function update ()
 {
-    r1.setAlpha((keys.A.isDown) ? 1 : 0.2);
-    r2.setAlpha((keys.W.isDown) ? 1 : 0.2);
-    r3.setAlpha((keys.D.isDown) ? 1 : 0.2);
+    keyPositions[0].setAlpha((keys.A.isDown) ? 1 : 0.2);
+    keyPositions[1].setAlpha((keys.W.isDown) ? 1 : 0.2);
+    keyPositions[2].setAlpha((keys.D.isDown) ? 1 : 0.2);
 
-    r4.setAlpha((keys.SPACE.isDown) ? 1 : 0.2);
+    keyPositions[3].setAlpha((keys.SPACE.isDown) ? 1 : 0.2);
     
-    r5.setAlpha((keys.J.isDown) ? 1 : 0.2);
-    r6.setAlpha((keys.I.isDown) ? 1 : 0.2);
-    r7.setAlpha((keys.L.isDown) ? 1 : 0.2);
+    keyPositions[4].setAlpha((keys.J.isDown) ? 1 : 0.2);
+    keyPositions[5].setAlpha((keys.I.isDown) ? 1 : 0.2);
+    keyPositions[6].setAlpha((keys.L.isDown) ? 1 : 0.2);
 
 }
 
@@ -96,3 +265,6 @@ function onYoyoHandler (tween, target)
     target.toggleFlipX().setAlpha(0.2 + Math.random());
 }
 
+function noteRead(noteSheet)
+{
+}
