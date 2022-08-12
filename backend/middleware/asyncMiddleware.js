004bb4a6388ec8b7e48b0160969452dba14e72ea
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-08-12 19:17:01 -0400

    signup route sending data to database

---
+++ backend/middleware/asyncMiddleware.js
@@ -0,0 +1,6 @@
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+module.exports = asyncMiddleware;
--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const asyncMiddleware = require('../middleware/asyncMiddleware');
+const UserModel = require('../models/userModel');
 
 const router = express.Router();
+
 
 router.get('/status', (req, res, next) => {
   res.status(200);
   res.json({ 'status': 'ok' });
 });
-router.post('/signup', (req, res, next) => {
-    res.status(200);
-    res.json({ 'status': 'ok' });
-  });
+
+//when the signup route is visited, we pull the name, email, and password fields from the request body and then we pass these arguments to the create method of our UserModel. By calling the create method on our model, mongoose will trigger the save pre-hook we set up, and once that is complete mongoose will attempt to add the new document to the database.
+router.post('/signup', asyncMiddleware( async (req, res, next) => {
+    const { name, email, password } = req.body;
+    await UserModel.create({ email, password, name });
+    res.status(200).json({ 'status': 'ok' });
+  }));
+
 router.post('/login', (req, res, next) => {
     res.status(200);
     res.json({ 'status': 'ok' });
   });
+
 router.post('/logout', (req, res, next) => {
     res.status(200);
     res.json({ 'status': 'ok' });
   });
+
 router.post('/token', (req, res, next) => {
     res.status(200);
     res.json({ 'status': 'ok' });
