ef597767bc43d815204cc79d5b99fbd14d634f07
Abhishek <abhishekism0010@gmail.com>
2023-01-23 13:07:27 +0530

    Removed 3rd party phaser plugin reference

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as Phaser from 'phaser';
 const logoImg =  require('./assets/logo.png');
 
-import ButtonPlugin from 'phaser3-rex-plugins/plugins/button-plugin.js';
 
 class MyGame extends Phaser.Scene
 {
@@ -22,48 +21,11 @@ class MyGame extends Phaser.Scene
             yoyo: true,
             loop: -1
         });
-        createBtn(this, {
-            x: 400,
-            y: 300,
-            color: 0x00cccc,
-            name: 'btn0'
-        });
-        createBtn(this, {
-            x: 390,
-            y: 360,
-            color: 0xcc00cc,
-            name: 'btn1'
-        });
-        createBtn(this, {
-            x: 480,
-            y: 280,
-            color: 0xcccc00,
-            name: 'btn2'
-        });
     }
 }
 
 const GetValue = Phaser.Utils.Objects.GetValue;
-var createBtn = function (scene: any, config: any) {
-    var x = GetValue(config, 'x', 0);
-    var y = GetValue(config, 'y', 0);
-    var color = GetValue(config, 'color', 0xffffff);
-    var name = GetValue(config, 'name', '');
 
-    var btn = scene.add.rectangle(x, y, 120, 120, color)
-        .setName(name);
-    scene.add.text(x, y, name, {
-        fontSize: '20pt'
-    })
-        .setOrigin(0.5, 0.5)
-    btn.button = scene.plugins.get('rexButton').add(btn, {
-        // clickInterval: 1000  // ms
-    });
-    btn.button.on('click', function (button: any, gameObject: any) {
-        scene.print.text += `click ${gameObject.name}\n`;
-    });
-    return btn;
-}
 
 
 const config = {
@@ -73,12 +35,8 @@ const config = {
     height: 600,
     scene: MyGame,
     plugins: {
-        global: [{
-            key: 'rexButton',
-            plugin: ButtonPlugin,
-            start: true
-        },
-        // ...
+        global: [
+            {}
         ]
     }
 };
