37632c6dd9996ca59893e29dd41c6087c25ef261
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-04-14 15:43:44 +0200

    unlock next level

--- src/components/level.js
+++ src/components/level.js
@@ -1,18 +1,21 @@
 // Define the Level class globally
 class Level {
-    constructor(name, difficulty, scene, unlocked, mapName) {
-      this.name = name;
-      this.difficulty = difficulty;
-      this.scene = scene;
-      this.unlocked = unlocked;
-      this.mapName = mapName;
-      this.completed = false;
-      this.score = 0;
+    constructor(id, name, difficulty, scene, unlocked, mapName) {
+        this.id = id;
+        this.name = name;
+        this.difficulty = difficulty;
+        this.scene = scene;
+        this.unlocked = unlocked;
+        this.mapName = mapName;
+        this.completed = false;
+        this.score = 0;
     }
   
     complete(score) {
       this.completed = true;
       this.score = score > this.score ? score : this.score;
+      // unlock next level
+        levels[this.id + 1].setUnlocked(true);
     }
 
     setUnlocked(unlocked){
--- src/config/levelsConfig.js
+++ src/config/levelsConfig.js
@@ -1,7 +1,7 @@
 // Define the levels array globally
 const levels = [
-    new Level('Hell', 'EASY', 'Level1Scene', true, 'level1'),
-    new Level('Nightmare', 'EASY', 'Level2Scene', false, 'level2'),
-    new Level('Neviem', 'MEDIUM', 'Level3Scene', false, 'level3'),
-    new Level('Dream', 'HARD', 'Level4Scene', false, 'level4'),
+    new Level(0, 'Hell', 'EASY', 'Level1Scene', true, 'level1'),
+    new Level(1, 'Nightmare', 'EASY', 'Level2Scene', false, 'level2'),
+    new Level(2, 'Neviem', 'MEDIUM', 'Level3Scene', false, 'level3'),
+    new Level(3, 'Dream', 'HARD', 'Level4Scene', false, 'level4'),
 ];
