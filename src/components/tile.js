cdd45f9457a91600d04a13fbf08431318770e949
Martin Mochnacký <martin.mochnacky@student.tuke.sk>
2023-04-14 18:30:50 +0200

    tile map fill

---
+++ src/components/tile.js
@@ -0,0 +1,10 @@
+class Tile {
+    constructor(id, x, showNum, tileType, finishedTile) {
+        this.id = id;
+        this.x = x;
+        this.tileType = showNum;
+        this.tileType = tileType;
+        this.finishedTile = finishedTile;
+    }
+
+}
