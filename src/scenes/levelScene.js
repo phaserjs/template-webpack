e7319f1bb0b1c2452fe79171cff747be476e06f1
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2023-04-15 09:19:05 +0200

    new spritesheet

--- src/scenes/levelScene.js
+++ src/scenes/levelScene.js
@@ -29,55 +29,16 @@ class levelScene extends Phaser.Scene {
         // create tiles
         this.setupTiles();
 
-        // set scrolling speed
-        const scrollSpeed = 2;
-            
-        // get reference to player object
-        // const player = this.add.sprite(400,700, 'menuBtn');
-        // player.setOrigin(0.5);
-        // // assign player to scene object for later use
-        // this.player = player;
-
-
-        // assign update function to scene object
-        // this.update = function () {
-        //     var cursors = this.input.keyboard.createCursorKeys();
-
-        //     if (cursors.left.isDown) {
-        //         this.player.x -= 5;
-        //     }
-        //     else if (cursors.right.isDown) {
-        //         this.player.x += 5;
-        //     }
-
-        //     if (cursors.up.isDown) {
-        //         this.player.y -= 5;
-        //     }
-        //     else if (cursors.down.isDown) {
-        //         this.player.y += 5;
-        //     }
-
-        //     if (this.player.y < prev) {
-        //         let temp = prev - player.y;
-                
-        //         background.tilePositionY -= temp;
-        //         prev = prev-temp;         
-        //     }
-
-        //     if (this.player.y > prevFix) {
-        //         prev=prevFix;
-        //     }
-         
-        // };
+        
         // Hodnoty na nastavenie rychlosti vyskoku adopadu
-        this.jumpSpedUp = 5;
-        this.jumpSpedDown = 2;
+        // this.jumpSpedUp = 5;
+        // this.jumpSpedDown = 2;
 
-        this.initTilesGenerator();
+        // this.initTilesGenerator();
        
 
-             // Iba na skusku
-            this.cursors = this.input.keyboard.createCursorKeys();
+        // // Iba na skusku
+        // this.cursors = this.input.keyboard.createCursorKeys();
               
     }
 
@@ -85,15 +46,15 @@ class levelScene extends Phaser.Scene {
     update() {
         
        //Iba na skusku
-        console.log(this.showNumber);
+        // console.log(this.showNumber);
 
-        if (this.cursors.up.isDown) {
-            this.moveDownTiles();
+        // if (this.cursors.up.isDown) {
+        //     this.moveDownTiles();
             
-        }
-        else if (this.cursors.down.isDown) {
-            this.moveUpTiles();
-        }
+        // }
+        // else if (this.cursors.down.isDown) {
+        //     this.moveUpTiles();
+        // }
        
     }
 
@@ -158,8 +119,8 @@ class levelScene extends Phaser.Scene {
     }
 
     setupTiles(){
-        for(let x = 0; x<= this.game.canvas.width; x = x+ (48 * 2)){
-            this.add.sprite(x, this.game.canvas.height, 'tileMap', 'grassTop').setOrigin(1, 1).setScale(2);
+        for(let x = 0; x<= this.game.canvas.width; x = x+16){
+            this.add.sprite(x, this.game.canvas.height, 'tileMap', 'grassTopMid').setOrigin(1, 1);
         }
     }
 
