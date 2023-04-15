a6ee1f811cb4c5d59d68dd62f307528893c130bf
Martin Mochnacký <martin.mochnacky@student.tuke.sk>
2023-04-15 17:27:36 +0200

    dokoncene generovanie a kolizie nedokoncene

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,10 +1,12 @@
 class Tile {
-    constructor(id, x ,y, showNum, tileType, tileLenght, finishedTile) {
-        this.id = id;
+    constructor(id, x ,y, showNum, tileType,tileTypeFirs,tileTypeLast,tileLenght, finishedTile) {
+        this.id = new Set();
         this.x = x;
         this.y=y;
         this.showNum = showNum;
         this.tileType = tileType;
+        this.tileTypeFirs = tileType;
+        this.tileTypeLast = tileType;
         this.tileLenght = tileLenght;
         this.finishedTile = finishedTile;
     }
