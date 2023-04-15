5c8254bcbf208d3bf6fa0d20720b0446e67c7c67
Martin Mochnacký <martin.mochnacky@student.tuke.sk>
2023-04-15 18:47:28 +0200

    oprava nacitania spike, kolizie pridane

--- src/config/levelsConfig.js
+++ src/config/levelsConfig.js
@@ -1,18 +1,30 @@
+//blockType:
+//dead =ak narazi umrie
+//bonus =ak dostane nejake body
+//finish =ak narazi koniec
+//standard =ak narazi nič
+
+//fallDown = nenastavuje na dlazdice, vykona ak spadol hrac;
+
+
+
+
 const level1TileMap = [
-  {x: 300, showNum: -32, tileType: 'groundDownMid',tileTypeFirst: 'groundDownLeft',tileTypeLast: 'groundDownRight',tileLenght:10, finishedTile: false},
-  {x: 300, showNum: 32, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 20, showNum: 100, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 0, showNum: 130, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 9, showNum: 140, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 50, showNum: 150, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 150, showNum: 200, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 31, showNum: 312, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 48, showNum: 390, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 28, showNum: 459, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 200, showNum: 497, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 800, showNum: 510, tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: false},
-  {x: 90, showNum: 514, tileType: 'standard',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5, finishedTile: true}
+  {x: 300, showNum: -32, blockType:'standard', tileType: 'groundDownMid',tileTypeFirst: 'groundDownLeft',tileTypeLast: 'groundDownRight',tileLenght:10,  },
+  {x: 300, showNum: 32, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 300, showNum: 150, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 0, showNum: 130, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 9, showNum: 140, blockType:'dead',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 50, showNum: 150, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 150, showNum: 200, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5,  },
+  {x: 31, showNum: 312,blockType:'standard', tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5 },
+  {x: 48, showNum: 390, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5 },
+  {x: 28, showNum: 459, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5 },
+  {x: 200, showNum: 497, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5},
+  {x: 800, showNum: 510, blockType:'standard',tileType: 'grassTopMid',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5},
+  {x: 90, showNum: 514,blockType:'finish', tileType: 'standard',tileTypeFirst: 'grassTopLeft',tileTypeLast: 'grassTopRight',tileLenght:5}
 ];
+
 
 const level2TileMap = [
   {x: 60, showNum: 10, tileType: 'standard', finishedTile: false},
