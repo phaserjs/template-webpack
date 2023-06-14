84fb48f9b75cb39346ed47246b22032705b61297
gbmatheus <gabrielmatheus2503@gmail.com>
2023-06-13 22:40:04 -0300

    feat: adicionado configuração inicial do jogo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+// import assetsMap from  './assets/assetsmap.png'
+import assetsMap from  './assets/tilemap_packed.png'
+import mapJson from './assets/map.json'
 
 class MyGame extends Phaser.Scene
 {
@@ -9,30 +12,26 @@ class MyGame extends Phaser.Scene
     }
 
     preload ()
-    {
-        this.load.image('logo', logoImg);
+    {   
+        console.log({assetsMap, mapJson})
+        this.load.image('tiles', assetsMap)
+        this.load.tilemapTiledJSON('map', mapJson)
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        const map = this.make.tilemap({ 'key': 'map'})
+        const tileset = map.addTilesetImage('tilemap_packed', 'tiles')
+
+        const ground = map.createLayer('ground', tileset, 0, 0)
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 650,
+    height: 650,
     scene: MyGame
 };
 
