19e9344032ab3de19ca2c86d8914598a55ff97bd
gbmatheus <gabrielmatheus2503@gmail.com>
2023-07-22 10:58:42 -0300

    added movement

---
+++ src/scenes/movement/index.js
@@ -0,0 +1,287 @@
+import Phaser from "phaser";
+import Score from "../../classes/score";
+import EventName from "../../consts/event-name";
+import DirectionPlayer from "../../consts/direction";
+import Text from "../../classes/text";
+import ArrowUp from "../../assets/buttons/Icon_ArrowUp.png";
+import ArrowDown from "../../assets/buttons/Icon_ArrowDown.png";
+import ArrowLeft from "../../assets/buttons/Icon_ArrowLeft.png";
+import ArrowRight from "../../assets/buttons/Icon_ArrowRight.png";
+import Play from "../../assets/buttons/Icon_Tube.png";
+import Trash from "../../assets/buttons/Icon_Trash.png";
+import directions from "../../consts/direction";
+
+export default class MainMenuScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "movement-scene", active: true });
+    this.buttons = [];
+    this.selectButtonIndex = 0;
+    this.steps = [];
+    this.executeStepsStatus = "WAIT";
+  }
+
+  init() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  preload() {
+    this.load.image("arrow_up", ArrowUp);
+    this.load.image("arrow_down", ArrowDown);
+    this.load.image("arrow_left", ArrowLeft);
+    this.load.image("arrow_right", ArrowRight);
+    this.load.image("play", Play);
+    this.load.image("trash", Trash);
+  }
+
+  create() {
+    const { width, height } = this.scale;
+    const scaleButton = 0.2;
+    console.log({ width, height });
+
+    this.stepsImageObject = this.add.group();
+
+    const iconArrowUp = this.add
+      .image(50, 50, "arrow_up")
+      .setScale(scaleButton);
+    iconArrowUp.setName(directions.up);
+    iconArrowUp.setInteractive();
+
+    const iconArrowDown = this.add
+      .image(
+        iconArrowUp.x + iconArrowUp.displayWidth + 20,
+        iconArrowUp.y,
+        "arrow_down"
+      )
+      .setScale(scaleButton);
+    iconArrowDown.setName(directions.down);
+    iconArrowDown.setInteractive();
+
+    const iconArrowLeft = this.add
+      .image(
+        iconArrowDown.x + iconArrowDown.displayWidth + 20,
+        iconArrowDown.y,
+        "arrow_left"
+      )
+      .setScale(scaleButton);
+    iconArrowLeft.setName(directions.left);
+    iconArrowLeft.setInteractive();
+
+    const iconArrowRight = this.add
+      .image(
+        iconArrowLeft.x + iconArrowLeft.displayWidth + 20,
+        iconArrowLeft.y,
+        "arrow_right"
+      )
+      .setScale(scaleButton);
+    iconArrowRight.setName(directions.right);
+    iconArrowRight.setInteractive();
+
+    const iconPlay = this.add.image(550, 520, "play").setScale(scaleButton);
+    iconPlay.setName("play");
+    iconPlay.setInteractive({ pixelPerfect: true });
+    iconPlay
+      .on("pointerover", () => {
+        console.log("play over");
+        iconPlay.setTint(0xf00000);
+      })
+      .on("pointerout", () => {
+        console.log("play out");
+        iconPlay.setTint();
+      })
+      .on("pointerdown", () => {
+        console.log("play click");
+        iconPlay.setTint(0x66ff7f);
+        this.game.events.emit(EventName.executeSteps, "EXECUTE", {
+          steps: this.steps,
+        });
+        // this.scene.start('loading-scene')
+      });
+
+    const iconDelete = this.add.image(550, 590, "trash").setScale(scaleButton);
+    iconDelete.setName("delete");
+    iconDelete.setInteractive({ pixelPerfect: true });
+    iconDelete
+      .on("pointerover", () => {
+        console.log("delete over");
+        iconDelete.setTint(0xf00000);
+      })
+      .on("pointerout", () => {
+        console.log("delete out");
+        iconDelete.setTint();
+      })
+      .on("pointerdown", () => {
+        console.log("delete click");
+        iconDelete.setTint(0x66ff7f);
+        this.game.events.emit(EventName.executeSteps, "STOP", { steps: [] });
+        // this.scene.start('loading-scene')
+      });
+
+    // this.input.setDraggable(iconArrowUp)
+    this.input.setDraggable([
+      iconArrowUp,
+      iconArrowDown,
+      iconArrowLeft,
+      iconArrowRight,
+    ]);
+    console.log({ iconArrowUp });
+    const container = this.add.container(20, 50, [
+      iconArrowUp,
+      iconArrowDown,
+      iconArrowLeft,
+      iconArrowRight,
+    ]);
+
+    // zona de largar o botão
+    const zone = this.add.zone(100, 500, 400, 200).setDropZone();
+    //  Just a visual display of the drop zone
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0xffff00);
+    graphics.strokeRect(
+      zone.x + zone.input.hitArea.x,
+      zone.y + zone.input.hitArea.y,
+      zone.input.hitArea.width,
+      zone.input.hitArea.height
+    );
+
+    this.input.on("dragstart", (pointer, gameObject) => {
+      gameObject.setTint(0xff0000);
+    });
+
+    // this.input.on('dragstart', function (pointer, gameObject)
+    // {
+    //     // coloca o ultimo objeto/image/sprite no topo
+    //     this.children.bringToTop(gameObject);
+
+    // }, this);
+
+    this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    });
+
+    this.input.on("dragenter", (pointer, gameObject, dropZone) => {
+      graphics.clear();
+      graphics.lineStyle(2, 0x00ffff);
+      graphics.strokeRect(
+        zone.x + zone.input.hitArea.x,
+        zone.y + zone.input.hitArea.y,
+        zone.input.hitArea.width,
+        zone.input.hitArea.height
+      );
+    });
+
+    this.input.on("dragend", (pointer, gameObject) => {
+      gameObject.clearTint();
+    });
+
+    this.input.on("dragleave", (pointer, gameObject, dropZone) => {
+      graphics.clear();
+      graphics.lineStyle(2, 0xffff00);
+      graphics.strokeRect(
+        zone.x + zone.input.hitArea.x,
+        zone.y + zone.input.hitArea.y,
+        zone.input.hitArea.width,
+        zone.input.hitArea.height
+      );
+    });
+
+    this.input.on("drop", (pointer, gameObject, dropZone) => {
+      let x = dropZone.x;
+      console.log({ dropZoneX: dropZone.x });
+      if (this.steps.length > 0) {
+        console.log({ displayWidth: gameObject.displayWidth });
+        x = dropZone.x + (this.steps.length * gameObject.displayWidth + 20);
+      }
+
+      console.log({ steps: this.steps });
+      // console.log({gameObject})
+      // gameObject.x = x;
+      // gameObject.y = dropZone.y;
+      gameObject.clearTint();
+      gameObject.x = gameObject.input.dragStartX;
+      gameObject.y = gameObject.input.dragStartY;
+
+      // gameObject.input.enabled = false;
+      // const container = this.add.container(20, 50, [iconArrowUp, iconArrowDown, iconArrowLeft, iconArrowRight]).add
+
+      let stepImageObject = this.add
+        .image(x, dropZone.y, gameObject.texture.key)
+        .setScale(scaleButton)
+        .on("pointerdown", (event) => {
+          console.log(event);
+          //  Then destroy it. This will fire a 'destroy' event that the Group will hear
+          //  and then it'll automatically remove itself from the Group.
+          console.log("stepsImageObject before delete step ", {
+            stepImageObject: this.stepsImageObject.getChildren(),
+          });
+          stepImageObject.destroy();
+          console.log("stepsImageObject after delete step ", {
+            stepsImageObject: this.stepsImageObject.getChildren(),
+          });
+        })
+        .setInteractive();
+
+      this.steps.push(gameObject.name);
+      // this.stepsImageObject.add(stepImageObject.getChildren())
+      this.stepsImageObject.add(stepImageObject);
+      console.log("stepsImageObject after add step ", {
+        stepsImageObject: this.stepsImageObject.getChildren(),
+      });
+    });
+
+    this.input.on("dragend", (pointer, gameObject, dropped) => {
+      console.log(dropped);
+      if (!dropped) {
+        gameObject.clearTint();
+        gameObject.x = gameObject.input.dragStartX;
+        gameObject.y = gameObject.input.dragStartY;
+      }
+
+      graphics.clear();
+      graphics.lineStyle(2, 0xffff00);
+      graphics.strokeRect(
+        zone.x + zone.input.hitArea.x,
+        zone.y + zone.input.hitArea.y,
+        zone.input.hitArea.width,
+        zone.input.hitArea.height
+      );
+    });
+
+    this.initListeners();
+  }
+
+  executeStepsHandler(event) {
+    console.log("Movement executeSH " + event);
+    console.log({ stepsImageObject: this.stepsImageObject });
+    if (event === "STOP") {
+      this.steps = [];
+      this.executeStepsStatus = "STOP";
+    }
+  }
+
+  initListeners() {
+    this.game.events.on(EventName.executeSteps, this.executeStepsHandler, this);
+  }
+
+  update() {
+    if (this.executeStepsStatus == "STOP" && this.steps.length === 0) {
+      this.stepsImageObject.getChildren().forEach((image) => {
+        // this.stepsImageObject.children.entries.forEach(image => {
+        image.destroy();
+      });
+
+      if (this.stepsImageObject.getChildren().length === 0)
+        this.executeStepsStatus = "WAIT";
+    }
+    // const upJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.up)
+    // const downJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.down)
+    // const spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space)
+
+    // if(upJustPressed)
+    //   this.selectNextButton(-1)
+    // else if(downJustPressed)
+    //   this.selectNextButton(1)
+    // else if(spaceJustPressed)
+    //   this.confirmSelection()
+  }
+}
