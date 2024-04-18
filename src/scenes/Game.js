62fb22574ece2c7e0b3fd8bcc46506452ca71614
David Yarbrough <david@davidyarbrough.net>
2024-04-17 23:38:56 -0500

    adding a basic card generator. Plus I found a cool fadein effect so I added that

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,9 +2,55 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
+    numbers = [1, 2, 3]
+    colors = ["red", "green", "purple"]
+    symbols = ["diamond", "pill", "squiggly"]
+    fills = ["hollow", "shaded", "filled"]
+    
+    gridConfiguration = {
+        x: 113,
+        y: 102,
+        paddingX: 10,
+        paddingY: 10
+    }
+
     constructor ()
     {
         super('Game');
+    }
+
+    init ()
+    {
+        // Fadein camera
+        this.cameras.main.fadeIn(500);
+        this.lives = 10;
+    }
+
+
+    newRandomCard() {
+        var numberIndex = Math.floor(Math.random() * this.numbers.length)
+        var colorIndex = Math.floor(Math.random() * this.colors.length)
+        var symbolIndex = Math.floor(Math.random() * this.symbols.length)
+        var fillIndex = Math.floor(Math.random() * this.numbers.length)
+        var newCard = {
+            number: this.numbers[numberIndex],
+            color: this.colors[colorIndex],
+            symbol: this.symbols[symbolIndex],
+            fill: this.fills[fillIndex],
+        }
+
+        return newCard
+
+    }
+
+    createNewGrid() {
+        var numberOfCards = 12 //TODO extract to config or something
+        var cards = []
+
+        for (var i = 0; i < numberOfCards; i++) {
+            cards.push(this.newRandomCard())
+        }
+        return cards
     }
 
     create ()
@@ -12,8 +58,8 @@ export class Game extends Scene
         this.cameras.main.setBackgroundColor(0x00ff00);
 
         this.add.image(512, 384, 'background').setAlpha(0.5);
-
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+        console.log(this.createNewGrid())
+        this.add.text(512, 384, "Test", {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,15 +7,25 @@ export class MainMenu extends Scene
         super('MainMenu');
     }
 
+    init ()
+    {
+        // Fadein camera
+        this.cameras.main.fadeIn(500);
+        this.lives = 10;
+    }
+
     create ()
     {
         this.add.image(512, 384, 'background');
 
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.text(512, 460, 'Press Start', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+        this.add.text(512, 510, '                      ^ this start', {
+            fontFamily: 'Arial Black', fontSize: 28, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 6,
             align: 'center'
         }).setOrigin(0.5);
 
